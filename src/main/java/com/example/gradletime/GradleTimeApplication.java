39ff04e12d3c502292a29d2d17bc26402ef461fe
Austyn Harriman <aeharrima@gmail.com>
2023-08-04 14:11:01 -0500

    first

---
+++ src/main/java/com/example/gradletime/GradleTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradletime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gradletime/GradleTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradletime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
