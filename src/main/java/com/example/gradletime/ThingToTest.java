9a2653dcbb4981beecdcfbb5261a094a3ae00c7a
Austyn Harriman <aeharrima@gmail.com>
2023-08-04 14:23:11 -0500

    trying again

--- src/main/java/com/example/gradletime/ThingToTest.java
+++ src/main/java/com/example/gradletime/ThingToTest.java
@@ -3,6 +3,7 @@ package com.example.gradletime;
 public class ThingToTest {
 
     public boolean testMe() {
+        System.out.println("adding a thing to try pushing again");
         return false;
     }
 
